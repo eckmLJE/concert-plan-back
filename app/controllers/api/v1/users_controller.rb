5232fded49c656079a3913becb0c5431e089b5d2
eckmLJE <lucas.eckman@gmail.com>
2018-08-08 10:10:07 -0400

    jwt fizzbuzz

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::UsersController < ApplicationController
 
-    before_action :authenticate_v1_user
+    before_action :authenticate_user, only: [:show_user]
 
     def index
         render json: User.all
+    end
+
+    def show_user
+        render json: current_user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'user_token' => 'user_token#create'
+      get 'user' => 'users#show_user'
       resources :comments
       resources :plan_users
       resources :users
